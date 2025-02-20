586ac5f34bffc3c893cd912c2be39f7d8d510f34
Rustamkhonov Shokhrukhkhon <93682008+rustamkhonoff@users.noreply.github.com>
2025-02-20 14:58:38 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,32 @@
 import os
 import asyncio
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram.filters import Command
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
 URL = "https://avo-quiz-pub.vercel.app"
 
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
 
-@dp.message_handler(commands=['start'])
-async def start(message: types.Message):
-    keyboard = InlineKeyboardMarkup()
-    keyboard.add(InlineKeyboardButton("🎮 Запустить!", url=URL))
-    await message.answer("Нажмите кнопку ниже, чтобы начать!:", reply_markup=keyboard)
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(Command("start"))
+async def start(message: Message):
+    keyboard = InlineKeyboardMarkup(inline_keyboard=[
+        [InlineKeyboardButton(text="🎮 Запустить!", url=URL)]
+    ])
+    await message.answer("Нажмите кнопку ниже, чтобы начать!", reply_markup=keyboard)
 
 async def main():
-    await dp.start_polling()
+    dp.include_router(dp)
+    
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
