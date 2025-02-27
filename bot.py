1936ebf01bea9ab4f356f5cf4decfff8b7218cac
Rustamkhonov Shokhrukhkhon <93682008+rustamkhonoff@users.noreply.github.com>
2025-02-27 22:28:34 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,7 @@
 import os
 import asyncio
+import json
+import urllib.parse
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message, ReplyKeyboardMarkup, KeyboardButton, WebAppInfo
 from aiogram.filters import Command
@@ -8,7 +10,7 @@ from dotenv import load_dotenv
 # Загружаем переменные окружения
 load_dotenv()
 
-# Берём токен и URL из .env
+# Берем токен и URL из .env
 TOKEN = os.getenv("TOKEN")
 BASE_URL = os.getenv("BASE_URL")  # Читаем Web App URL из .env
 
@@ -22,13 +24,27 @@ dp = Dispatcher()
 
 @dp.message(Command("start"))
 async def start(message: Message):
-    """Обрабатывает команду /start и отправляет Web App кнопку"""
-    username = message.from_user.username or "Guest"
-    first_name = message.from_user.first_name or "Unknown"
-    user_id = message.from_user.id
+    """Обрабатывает команду /start и отправляет Web App кнопку со всеми данными о пользователе"""
 
-    # Формируем ссылку с параметрами
-    user_url = f"{BASE_URL}?user={message.from_user}&name={first_name}&id={user_id}"
+    # Получаем все данные из from_user
+    user_data = {
+        "id": message.from_user.id,
+        "is_bot": message.from_user.is_bot,
+        "first_name": message.from_user.first_name,
+        "last_name": message.from_user.last_name,
+        "username": message.from_user.username,
+        "language_code": message.from_user.language_code,
+        "can_join_groups": message.from_user.can_join_groups,
+        "can_read_all_group_messages": message.from_user.can_read_all_group_messages,
+        "supports_inline_queries": message.from_user.supports_inline_queries
+    }
+
+    # Конвертируем данные в JSON и кодируем для передачи в URL
+    user_json = json.dumps(user_data)
+    encoded_user_json = urllib.parse.quote(user_json)
+
+    # Формируем ссылку с JSON параметром
+    user_url = f"{BASE_URL}?user_data={encoded_user_json}"
 
     # Создаём клавиатуру с Web App кнопкой
     keyboard = ReplyKeyboardMarkup(
